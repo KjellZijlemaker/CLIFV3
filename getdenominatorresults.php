97436c4b7fb4b354e7dc1793150cba361ea2fa23
Kjell Zijlemaker <kjell.zijlemaker@live.nl>
2020-02-10 08:44:21 +0100

    Update

--- getdenominatorresults.php
+++ getdenominatorresults.php
@@ -4,13 +4,10 @@ require_once 'inc/queryutil.inc';
 $DEBUG_MODE = false;
 session_start();
 
-
-// $json_results_num = json_encode($_SESSION["numeratorresults"] );
-// $json_results_den = json_encode($_SESSION["denominatorresults"] );
-
 //we now have the unique patient id's
 $difference = array_values(array_diff(array_column($_SESSION["denominatorresults"], 'patientid'), array_column($_SESSION["numeratorresults"], 'patientid' )));
 
+//only save these patients in a new array
 $newarr = [];
 foreach($_SESSION["denominatorresults"] as $value){
     foreach($difference as $diff){
